0f6329dc906428c5843e3c7d22eac4bea48741aa
Valeriia <vomelchenko1907@gmail.com>
2023-08-02 21:37:37 +0300

    Bluetooth connected

--- EmbeddedPart/SmartStorage/SmartStorage.ino
+++ EmbeddedPart/SmartStorage/SmartStorage.ino
@@ -1,9 +1,16 @@
+int data;
 void setup() {
   // put your setup code here, to run once:
+ Serial.begin(9600);
+  Serial3.begin(9600);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+if (Serial3.available()) {
+    data = Serial3.parseInt();
+    Serial.println(data);
+    delay(10000);
+}
 }
