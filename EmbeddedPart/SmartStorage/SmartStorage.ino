43424979665e09ae9114e9ac9d4dc2c4f0bd8d50
Valeriia <vomelchenko1907@gmail.com>
2023-08-02 22:24:59 +0300

    LED selection

--- EmbeddedPart/SmartStorage/SmartStorage.ino
+++ EmbeddedPart/SmartStorage/SmartStorage.ino
@@ -1,16 +1,31 @@
 int data;
+byte currentLED;
+byte minLED=2;
+byte maxLED=5;
 void setup() {
   // put your setup code here, to run once:
- Serial.begin(9600);
+  Serial.begin(9600);
   Serial3.begin(9600);
-
+  for (byte i=minLED;i<=maxLED;i++){
+      pinMode(i, OUTPUT);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-if (Serial3.available()) {
+  if (Serial3.available()) {
     data = Serial3.parseInt();
+    if((data>=minLED)&&(data<=maxLED)||(data==0)){
+    if ((currentLED != data)&&(data!=0)) {
+digitalWrite(currentLED, LOW);
+      currentLED = data;
+    }
     Serial.println(data);
-    delay(10000);
-}
+    digitalWrite(data, HIGH);
+    delay(1000);
+    }
+    else{
+      Serial3.print(99);
+    }
+  }
 }
